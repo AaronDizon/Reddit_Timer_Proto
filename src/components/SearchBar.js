341a77b8dd800a2ff8d85808da015e71641cde57
Aaron <imartindizon@gmail.com>
2022-08-19 18:52:42 -0700

    fixed obtaining the next after in reddit request

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -12,7 +12,6 @@ const SearchBar = () => {
 
     const [description, setDescription] = useState('')
     const [loaded, setLoaded] = useState(false)
-    const [after, setAfter] = useState('')
 
     const postsPerRequest = 100
     const maxPostsToFetch = 500
@@ -20,21 +19,17 @@ const SearchBar = () => {
 
     const getInfo = async (e) => {
         try {
+            let tempString = ''
+            setInfoSearched([])
             for(let i = 0; i < maxRequests; i++){
-                let tempAfter
-                setAfter('lol')
-            console.log(after)
-            await axios.get(`https://www.reddit.com/r/${description}.json?limit=${postsPerRequest}&after=${after}`)
-                .then((response) => {
-                    console.log(response.data.data)
-                    console.log(response.data.data.after)
-                    tempAfter = response.data.data.after
-                    console.log(typeof tempAfter)
-                    console.log(after)
-                })
-                console.log(tempAfter)
-                setAfter(tempAfter)
-                console.log(after)
+                console.log(tempString)
+                await axios.get(`https://www.reddit.com/r/${description}.json?limit=${postsPerRequest}&after=${tempString}`)
+                    .then((response) => {
+                        // console.log(response.data.data)
+                        console.log(response.data.data.children)
+                        tempString = response.data.data.after
+                        setInfoSearched()
+                    })
             }
             
         } catch (error) {
@@ -42,7 +37,7 @@ const SearchBar = () => {
         }
     }
 
-    const postForm = async(e) => {
+    const postForm = (e) => {
         e.preventDefault()
         getInfo()
         setInfoSearchStatus(true)
