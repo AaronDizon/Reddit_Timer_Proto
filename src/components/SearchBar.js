5f4a711c0d06bd23a53aaaff29bb25ecbe178620
Aaron <imartindizon@gmail.com>
2022-08-19 17:46:56 -0700

    post request wip

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -12,7 +12,7 @@ const SearchBar = () => {
 
     const [description, setDescription] = useState('')
     const [loaded, setLoaded] = useState(false)
-    const [after, setAfter] = useState('nothing')
+    const [after, setAfter] = useState('')
 
     const postsPerRequest = 100
     const maxPostsToFetch = 500
@@ -21,14 +21,22 @@ const SearchBar = () => {
     const getInfo = async (e) => {
         try {
             for(let i = 0; i < maxRequests; i++){
-            await axios.get(`https://www.reddit.com/r/${description}.json?limit=${postsPerRequest}${
-                after !== 'nothing'? '&after=' + after : ''}`)
+                let tempAfter
+                setAfter('lol')
+            console.log(after)
+            await axios.get(`https://www.reddit.com/r/${description}.json?limit=${postsPerRequest}&after=${after}`)
                 .then((response) => {
                     console.log(response.data.data)
                     console.log(response.data.data.after)
-                    setAfter(response.data.data.after)
+                    tempAfter = response.data.data.after
+                    console.log(typeof tempAfter)
+                    console.log(after)
                 })
+                console.log(tempAfter)
+                setAfter(tempAfter)
+                console.log(after)
             }
+            
         } catch (error) {
             console.log(error)
         }
