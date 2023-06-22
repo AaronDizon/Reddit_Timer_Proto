be1c3ca64e071deceac1ca0d0e0e8fc349e071c1
Aaron <imartindizon@gmail.com>
2023-06-22 11:51:07 -0700

    fixed index.js, removed react-router-dom

--- src/index.js
+++ src/index.js
@@ -4,16 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { SearchProvider } from './context/SearchContext';
-import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   
-    <BrowserRouter>
       <SearchProvider>
         <App />
       </SearchProvider>
-    </BrowserRouter>
  
 );
 
